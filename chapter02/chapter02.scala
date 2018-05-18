d1660c6c9974626758fc9eb60c911544f885ea26
Grigory Bordyugov <gregbg@gmail.com>
2018-05-18 18:20:50 +0200

    exercise 2.4 done

--- chapter02/chapter02.scala
+++ chapter02/chapter02.scala
@@ -79,10 +79,19 @@ object Chapter2 {
   def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
+
   /*
    * Exercise 2.3
    */
 
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
     (a: A) => ((b: B) => f(a, b))
+
+
+  /*
+   * Exercise 2.4
+   */
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
 }
