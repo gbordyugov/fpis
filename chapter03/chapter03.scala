ab368c799059f8d5912ecb6312a74d168a0be118
Grisha Bordyugov <gregbg@gmail.com>
2018-05-20 14:04:52 +0200

    some reformatting

--- chapter03/chapter03.scala
+++ chapter03/chapter03.scala
@@ -14,17 +14,18 @@ object Chapter3 {
       case Nil => 0
       case Cons(x, xs) => x + sum(xs)
     }
-  }
 
-  /*
-   * Exercise 3.1
-   */
-  import List.sum
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
+
+    /*
+     * Exercise 3.1
+     */
+
+    val x = List(1,2,3,4,5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + sum(t)
+      case _ => 101
+    }
   }
 }
