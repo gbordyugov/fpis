3813fd9ef85ac0b2314907f2379d5f0860d20e3e
Grigory Bordyugov <gregbg@gmail.com>
2018-05-23 17:11:07 +0200

    Option.flatMap()

--- chapter04/Option.scala
+++ chapter04/Option.scala
@@ -7,6 +7,11 @@ object ChapterOption {
       case None => None
       case Some(x) => Some(f(x))
     }
+
+    def flatMap[B](f: A => Option[B]): Option[B] = this match {
+      case None => None
+      case Some(x) => f(x)
+    }
   }
 
   case class Some[+A](get: A) extends Option[A]
