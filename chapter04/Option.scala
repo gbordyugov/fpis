c16212c9814788c5d898edd9637a117fe5cfdbcc
Grigory Bordyugov <gregbg@gmail.com>
2018-05-23 17:10:46 +0200

    Option.map()

--- chapter04/Option.scala
+++ chapter04/Option.scala
@@ -1,2 +1,14 @@
-package ChapterOption {
+object ChapterOption {
+  sealed trait Option[+A] {
+    /*
+     * Exercise 4.1
+     */
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(x) => Some(f(x))
+    }
+  }
+
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
 }
