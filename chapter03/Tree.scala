1a463f28dafc3870c8756f3b024820dcd1f15d17
Grigory Bordyugov <gregbg@gmail.com>
2018-05-23 14:24:56 +0200

    Tree.fold()

--- chapter03/Tree.scala
+++ chapter03/Tree.scala
@@ -42,4 +42,14 @@ object TreeChapter{
     case Leaf(x)      => Leaf(f(x))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
+
+
+  /*
+   * Exercise 3.29
+   */
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+		case Leaf(a) => f(a)
+		case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+	}
 }
