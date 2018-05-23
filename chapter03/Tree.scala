858a7cbcf1d101472480db5962d83da11eae6d44
Grigory Bordyugov <gregbg@gmail.com>
2018-05-23 14:13:58 +0200

    reformatting

--- chapter03/Tree.scala
+++ chapter03/Tree.scala
@@ -1,15 +1,25 @@
 object TreeChapter{
-	sealed trait Tree[+A]
-	case class Leaf[A](value: A) extends Tree[A]
-	case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
-	/*
- 	* Exercise 3.25
- 	*/
+  /*
+   * Exercise 3.25
+   */
 
-	def size[A](t: Tree[A]): Long = t match {
-		case Leaf(v) => 1
-		case Branch(l, r) => size(l) + size(r)
-	}
+  def size[A](t: Tree[A]): Long = t match {
+    case Leaf(v) => 1
+    case Branch(l, r) => size(l) + size(r)
+  }
+
+
+  /*
+   * Exercise 3.26
+   */
+
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(x)      => x
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 }
