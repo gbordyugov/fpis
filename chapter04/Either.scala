3d5db85fc0ddd827e50247b2324c8c5ed1b6f486
Grigory Bordyugov <gregbg@gmail.com>
2018-05-25 16:07:39 +0200

    Either.flatMap()

--- chapter04/Either.scala
+++ chapter04/Either.scala
@@ -1,9 +1,23 @@
 object ChapterEither {
   sealed trait Either[+E, +A] {
+
+
+    /*
+     * Exercise 4.6
+     */
+
     def map[B](f: A => B): Either[E, B] = this match {
       case Right(a) => Right(f(a))
       case Left(b)  => Left(b)
     }
+
+    def flatMap[EE >:E, B](f: A => Either[EE, B]): Either[EE, B] =
+      this match {
+        case Right(a) => f(a)
+        case Left(b)  => Left(b)
+      }
+
+
   }
 
   case class Left[+E] (value: E) extends Either[E, Nothing]
