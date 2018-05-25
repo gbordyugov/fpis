3d7f1cf5cee99611ff24a595fa9e7aabb91f25e7
Grigory Bordyugov <gregbg@gmail.com>
2018-05-25 17:29:32 +0200

    reading Chapter 5

--- chapter05/StrictnessAndLaziness.scala
+++ chapter05/StrictnessAndLaziness.scala
@@ -4,4 +4,28 @@ object StrictnessAndLazyness {
       onTrue()
     else
       onFalse()
+
+  def if2_[A](cond: Boolean, onTrue: => A, onFalse: => A):  A =
+    if (cond)
+      onTrue
+    else
+      onFalse
+
+  def maybeTwice(b: Boolean, i: => Int) =
+    if (b)
+      i + i
+    else
+      0
+
+  def maybeTwice2(b: Boolean, i: => Int) = {
+    /*
+     * `lazy` keword delays the evaluation of the rhs of that lazy
+     * declaration and caches the result of the evaluation
+     */
+    lazy val j = i
+    if (b)
+      j + j
+    else
+      0
+  }
 }
