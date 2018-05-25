855b9f30212bf4473eb63abe219ac3f9df0fe2e4
Grigory Bordyugov <gregbg@gmail.com>
2018-05-25 16:02:28 +0200

    Either.map()

--- chapter04/Either.scala
+++ chapter04/Either.scala
@@ -1,5 +1,9 @@
 object ChapterEither {
   sealed trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Right(a) => Right(f(a))
+      case Left(b)  => Left(b)
+    }
   }
 
   case class Left[+E] (value: E) extends Either[E, Nothing]
