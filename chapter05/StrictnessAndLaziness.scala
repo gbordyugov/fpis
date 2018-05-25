8652d00ad3e9b7a458c299398099ea56ee6f47db
Grigory Bordyugov <gregbg@gmail.com>
2018-05-25 17:35:31 +0200

    Stream.headOption

--- chapter05/StrictnessAndLaziness.scala
+++ chapter05/StrictnessAndLaziness.scala
@@ -29,7 +29,12 @@ object StrictnessAndLazyness {
       0
   }
 
-  sealed trait Stream[+A]
+  sealed trait Stream[+A] {
+    def headOption: Option[A] = this match {
+      case Empty => None
+      case Cons(h, t) => Some(h()) // explicit forcing
+    }
+  }
   case object Empty extends Stream[Nothing]
   // the explicit thunks are a technical necessity here
   case class Cons[+A](h: () => A, t: () => Stream[A])
