e25dbeb6b7371b4166234464e2678215292ddf21
Grigory Bordyugov <gregbg@gmail.com>
2018-05-25 17:34:12 +0200

    Chapter 5: class Stream

--- chapter05/StrictnessAndLaziness.scala
+++ chapter05/StrictnessAndLaziness.scala
@@ -28,4 +28,26 @@ object StrictnessAndLazyness {
     else
       0
   }
+
+  sealed trait Stream[+A]
+  case object Empty extends Stream[Nothing]
+  // the explicit thunks are a technical necessity here
+  case class Cons[+A](h: () => A, t: () => Stream[A])
+    extends Stream[A]
+
+  object Stream {
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+    }
+
+    def empty[A]: Stream[A] = Empty
+
+    def apply[A](as: A*): Stream[A] =
+      if (as.isEmpty)
+        empty
+      else
+        cons(as.head, apply(as.tail: _*))
+    }
 }
