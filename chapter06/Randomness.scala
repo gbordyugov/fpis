947f2fe6d5879a04eb691a09d5caf3473368316e
Grigory Bordyugov <gregbg@gmail.com>
2018-06-01 11:23:34 +0200

    SimpleRNG

--- chapter06/Randomness.scala
+++ chapter06/Randomness.scala
@@ -1,2 +1,20 @@
 object Randomness {
+  def rollDie: Int = {
+    val rng = new scala.util.Random
+    rng.nextInt(6)
+  }
+
+  trait RNG {
+    def nextInt: (Int, RNG)
+  }
+
+
+  case class SimpleRNG(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
 }
