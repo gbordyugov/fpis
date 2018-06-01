d93dec302cef24e3b91064b7810a1ee54e850545
Grigory Bordyugov <gregbg@gmail.com>
2018-06-01 18:17:38 +0200

    moving things around

--- chapter06/Randomness.scala
+++ chapter06/Randomness.scala
@@ -190,23 +190,23 @@ object Randomness {
    * Exercise 6.10
    */
 
-  case class State[S, +A](run: S => (A, S))
+  case class State[S, +A](run: S => (A, S)) {
+    def map[B](f: A => B): State[S, B] =
+      State { s1 => {
+        val (a, s2) = run(s1)
+        (f(a), s2)
+      }
+    }
+
+    def flatMap[B] (f: A => State[S, B]): State[S, B] =
+      State { s1 => {
+        val (a, s2) = run(s1)
+        f(a).run(s2)
+      }
+    }
+  }
 
   object State{
     def unit[S, A](a: A): State[S, A] = State(s => (a, s))
-
-    def map[S, A, B](sa: State[S, A])(f: A => B): State[S, B]
-      = State(s1 => {
-        val (a, s2) = sa.run(s1)
-        (f(a), s2)
-      }
-    )
-
-    def flatMap[S, A, B](sa: State[S, A])
-      (f: A => State[S, B]): State[S, B] = State( s1 => {
-      val (a, s2) = sa.run(s1)
-      f(a).run(s2)
-      }
-    )
   }
 }
