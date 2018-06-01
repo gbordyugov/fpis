9d9fd7b3c32ebbaa56e63c7c5d7d43c6df53170a
Grigory Bordyugov <gregbg@gmail.com>
2018-06-01 18:24:10 +0200

    Exercise 6.10 map2()

--- chapter06/Randomness.scala
+++ chapter06/Randomness.scala
@@ -208,5 +208,17 @@ object Randomness {
 
   object State{
     def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+    def map2[S, A, B, C](sa: State[S, A], sb: State[S, B])
+      (f: (A, B) => C): State[S, C] = State { s1 => {
+        val (a, s2) = sa.run(s1)
+        val (b, s3) = sb.run(s2)
+        (f(a, b), s3)
+      }
+    }
+    /*
+    def sequence[S, A](sas: List[State[S, A]): State[S, List[A]] =
+      sas.foldRight(unit[List[A]])((a, b) => map2(a, b)(_ :: _))
+    */
   }
 }
