5e59816d5354daf4190d77abe04ae42b371b8182
Grisha Bordyugov <gregbg@gmail.com>
2018-06-02 10:28:38 +0200

    Exercise 7.2 skipped

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # My read through some of the chapter of Functional Programming in Scala
+
+Open/skipped exercises:
+  - Exercise 7.2: I had no idea
--- chapter07/Parallel.scala
+++ chapter07/Parallel.scala
@@ -6,4 +6,9 @@ object Parallel {
    * Exercise 7.1
    */
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+
+
+  /*
+   * Exercise 7.2 skipped
+   */
 }
