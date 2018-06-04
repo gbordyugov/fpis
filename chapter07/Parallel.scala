3fee00655398cfa1fbd69185cd055f57ea4443f5
Grigory Bordyugov <gregbg@gmail.com>
2018-06-04 11:21:53 +0200

    fleshing out Par[A]

--- chapter07/Parallel.scala
+++ chapter07/Parallel.scala
@@ -1,14 +1,23 @@
-object Parallel {
-  trait Par[A]
+import java.util.concurrent.{ExecutorService, Future}
 
+object Parallel {
 
   /*
    * Exercise 7.1
    */
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+  def map2First[A, B, C](a: Par[A], b: Par[B])
+    (f: (A, B) => C): Par[C] = ???
 
 
   /*
    * Exercise 7.2 skipped
    */
+
+
+  /*
+   * Par[A] is parameterised by an ExecutorService
+   */
+  type Par[A] = ExecutorService => Future[A]
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 }
