818936da0975776e158cc21a501272d0aadf0eee
Grigory Bordyugov <gregbg@gmail.com>
2018-06-04 11:30:59 +0200

    fleshing out Par[A]

--- chapter07/Parallel.scala
+++ chapter07/Parallel.scala
@@ -1,4 +1,4 @@
-import java.util.concurrent.{ExecutorService, Future}
+import java.util.concurrent.{ExecutorService, Future, TimeUnit}
 
 object Parallel {
 
@@ -20,4 +20,25 @@ object Parallel {
   type Par[A] = ExecutorService => Future[A]
 
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+
+  object Par {
+    def unit[A](a: A): Par[A] =
+      (es: ExecutorService) => UnitFuture(a)
+
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      def isDone = true
+      def get(timeout: Long, units: TimeUnit) = get
+      def isCancelled = false
+      def cancel(evenIfRunning: Boolean): Boolean = false
+    }
+
+    def map2[A, B, C](a: Par[A], b: Par[B])
+      (f: (A, B) => C): Par[C] = (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+  }
 }
