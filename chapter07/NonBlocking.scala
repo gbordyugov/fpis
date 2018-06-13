82a9e76fc94a07b2c2c8f55c144a2283f29c2b66
Grigory Bordyugov <gregbg@gmail.com>
2018-06-13 15:16:02 +0200

    NonBlocking.run()

--- chapter07/NonBlocking.scala
+++ chapter07/NonBlocking.scala
@@ -1,4 +1,6 @@
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicReference
 
 object NonBlocking {
   sealed trait Future[A] {
@@ -6,4 +8,14 @@ object NonBlocking {
   }
 
   type Par[A] = ExecutorService => Future[A]
+
+  def run[A](es: ExecutorService)(p: Par[A]): A = {
+    val ref = new AtomicReference[A]
+    val latch = new CountDownLatch(1)
+    p(es) {
+      a => ref.set(a)
+      latch.countDown
+    }
+    ref.get
+  }
 }
