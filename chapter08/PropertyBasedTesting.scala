4696ec5ffae201994efc44d129464ff9baf6db67
Grigory Bordyugov <gregbg@gmail.com>
2018-06-19 17:19:58 +0200

    Exercise 8.1 done

---
+++ chapter08/PropertyBasedTesting.scala
@@ -0,0 +1,11 @@
+objecg PropertyBasedTesting {
+  /*
+   * Exercise 8.1
+   *
+   * sum of the reverse should be equal to the sum of the list
+   * sum of N repetitions of a number P should be equal NxP
+   * sum of an non-empty list should be equal to the sum of its head
+   *   plus the sum of the tail
+   * sum of a list should be equal to the sum of the sums of its partitions
+   */
+}
