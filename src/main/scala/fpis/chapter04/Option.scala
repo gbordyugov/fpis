e983e147f5e7c9ded0968143d6f46edb78fc49a3
Grigory Bordyugov <gregbg@gmail.com>
2018-06-21 11:48:14 +0200

    updated chapter04/Option.scala

--- src/main/scala/fpis/chapter04/Option.scala
+++ src/main/scala/fpis/chapter04/Option.scala
@@ -1,41 +1,43 @@
+package fpis.chapter04
+
 import math.pow
 
-object ChapterOption {
-  sealed trait Option[+A] {
-    /*
-     * Exercise 4.1
-     */
-    def map[B](f: A => B): Option[B] = this match {
-      case None => None
-      case Some(x) => Some(f(x))
-    }
-
-    def flatMap[B](f: A => Option[B]): Option[B] = this match {
-      case None => None
-      case Some(x) => f(x)
-    }
-
-    def getOrElse[B >: A](default: => B): B = this match {
-      case None    => default
-      case Some(x) => x
-    }
-
-    def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
-      case None    => ob
-      case Some(x) => Some(x)
-    }
-
-    def filter(f: A => Boolean): Option[A] = this match {
-      case None            => None
-      case Some(x) if f(x) => Some(x)
-      case _               => None
-    }
+sealed trait Option[+A] {
+  /*
+   * Exercise 4.1
+   */
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(x) => Some(f(x))
   }
 
-  case class Some[+A](get: A) extends Option[A]
-  case object None extends Option[Nothing]
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(x) => f(x)
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None    => default
+    case Some(x) => x
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None    => ob
+    case Some(x) => Some(x)
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case None            => None
+    case Some(x) if f(x) => Some(x)
+    case _               => None
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
 
 
+object Option {
   /*
    * Exercise 4.2
    */
