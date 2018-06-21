220d84865cfef8026cb99f77455a706b87559d36
Grigory Bordyugov <gregbg@gmail.com>
2018-06-21 11:45:23 +0200

    updated chapter03/Tree.scala

--- src/main/scala/fpis/chapter03/Tree.scala
+++ src/main/scala/fpis/chapter03/Tree.scala
@@ -1,11 +1,11 @@
 package fpis.chapter03
 
-object TreeChapter{
-  sealed trait Tree[+A]
-  case class Leaf[A](value: A) extends Tree[A]
-  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
+object Tree {
   /*
    * Exercise 3.25
    */
