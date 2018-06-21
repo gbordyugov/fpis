c60c6daf3accb6781c955cc0d1144b3002fe13c0
Grigory Bordyugov <gregbg@gmail.com>
2018-06-21 17:46:02 +0200

    Chapter 8 class Gen[A]

--- src/main/scala/fpis/chapter08/PropertyBasedTesting.scala
+++ src/main/scala/fpis/chapter08/PropertyBasedTesting.scala
@@ -1,41 +1,43 @@
 package fpis.chapter08
 
-object PropertyBasedTesting {
-  /*
-   * Exercise 8.1
-   *
-   * sum of the reverse should be equal to the sum of the list
-   * sum of N repetitions of a number P should be equal NxP
-   * sum of an non-empty list should be equal to the sum of its head
-   *   plus the sum of the tail
-   * sum of a list should be equal to the sum of the sums of its partitions
-   * sum of any permutation of the list should be the same
-   */
+import fpis.chapter06.{State, RNG}
 
-  /*
-   * Exercise 8.2
-   *
-   * maximum should not be smaller than any element of the list
-   * maximum of the reversed list should be equal to the maximum of
-   *   the list
-   * max of any permutation should be the same
-   * max of a non-empty list should be max(head, max(tail))
-   */
+/*
+ * Exercise 8.1
+ *
+ * sum of the reverse should be equal to the sum of the list
+ * sum of N repetitions of a number P should be equal NxP
+ * sum of an non-empty list should be equal to the sum of its head
+ *   plus the sum of the tail
+ * sum of a list should be equal to the sum of the sums of its partitions
+ * sum of any permutation of the list should be the same
+ */
 
-  trait Prop {
-    def check: Unit
-    def &&(p: Prop): Prop
-  }
+/*
+ * Exercise 8.2
+ *
+ * maximum should not be smaller than any element of the list
+ * maximum of the reversed list should be equal to the maximum of
+ *   the list
+ * max of any permutation should be the same
+ * max of a non-empty list should be max(head, max(tail))
+ */
 
-  /*
-   * Exercise 8.3
-   */
-  object Exercise83 {
-    trait Prop { outer =>
-      def check: Boolean
-      def &&(that: Prop): Prop = new Prop {
-        def check = outer.check && that.check
-      }
+trait Prop {
+  def check: Unit
+  def &&(p: Prop): Prop
+}
+
+/*
+ * Exercise 8.3
+ */
+object Exercise83 {
+  trait Prop { outer =>
+    def check: Boolean
+    def &&(that: Prop): Prop = new Prop {
+      def check = outer.check && that.check
     }
   }
 }
+
+case class Gen[A](sample: State[RNG, A])
