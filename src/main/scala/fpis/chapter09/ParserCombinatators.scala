04db0ecec384fa3d1c17b657cad13674e78e7e4c
Grigory Bordyugov <gregbg@gmail.com>
2018-08-17 11:02:10 +0200

    make many look like a member

--- src/main/scala/fpis/chapter09/ParserCombinatators.scala
+++ src/main/scala/fpis/chapter09/ParserCombinatators.scala
@@ -129,7 +129,7 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     def  |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
-    def many(): Parser[List[A]] = self.many(p)
+    def many: Parser[List[A]] = self.many(p)
     def product[B](p2: Parser[B]) = self.product(p, p2)
     def      **[B](p2: Parser[B]) = self.product(p, p2)
     def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
