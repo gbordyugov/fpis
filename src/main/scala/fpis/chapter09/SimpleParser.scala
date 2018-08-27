7d5fe1491fa32cf9d37a49ac0d04375bbf34c26d
Grigory Bordyugov <gregbg@gmail.com>
2018-08-27 18:46:29 +0200

    Chapter 9 stubs for SimpleParser()

--- src/main/scala/fpis/chapter09/SimpleParser.scala
+++ src/main/scala/fpis/chapter09/SimpleParser.scala
@@ -1,4 +1,7 @@
 package fpis.chapter09.SimpleParser
+
+import scala.util.matching.Regex
+
 import fpis.chapter09.Parsers
 
 class ParseError
@@ -7,5 +10,13 @@ case class SimpleParser[+A](p: String => ParseError) {
 }
 
 object TestingSimpleParser {
-  val parsers: Parsers[ParseError, SimpleParser] = ???
+  val parsers: Parsers[ParseError, SimpleParser] = new Parsers[ParseError, SimpleParser] {
+    def run[A](p: SimpleParser[A])(input: String): Either[ParseError, A] = ???
+    def delay[A](p: => SimpleParser[A]): SimpleParser[A] = ???
+    def flatMap[A, B](p: SimpleParser[A])(f: A => SimpleParser[B]): SimpleParser[B] = ???
+    def or[A](p: SimpleParser[A], q: => SimpleParser[A]): SimpleParser[A] = ???
+    def regex(r: Regex): SimpleParser[String] = ???
+    def slice[A](p: SimpleParser[A]): SimpleParser[String] = ???
+    implicit def string(s: String): SimpleParser[String] = ???
+  }
 }
