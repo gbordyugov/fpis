30fd86ca0d97b571528d7f68972df5483a14eb8e
Grisha Bordyugov <gregbg@gmail.com>
2018-08-30 21:33:47 +0200

    Chapter 9 some improvements

--- src/main/scala/fpis/chapter09/SExpParser.scala
+++ src/main/scala/fpis/chapter09/SExpParser.scala
@@ -6,8 +6,12 @@ import fpis.chapter09.Parsers
 
 sealed trait Atom
 object Atom {
-  case class Int(i: Int) extends Atom
-  case class String(s: String) extends Atom
+  case class AInt(i: Int) extends Atom
+  case class AString(s: String) extends Atom
+}
+object AtomTest {
+  val i = Atom.AInt(3)
+  val s = Atom.AString("string")
 }
 
 sealed trait SExp[A] {
