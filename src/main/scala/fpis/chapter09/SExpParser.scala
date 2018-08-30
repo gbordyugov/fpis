1b239e5ca2594fe834a0579e57f02083a1c2a18a
Grisha Bordyugov <gregbg@gmail.com>
2018-08-30 21:21:15 +0200

    Chapter 9 SExp

--- src/main/scala/fpis/chapter09/SExpParser.scala
+++ src/main/scala/fpis/chapter09/SExpParser.scala
@@ -4,6 +4,10 @@ import scala.util.matching.Regex
 
 import fpis.chapter09.Parsers
 
+trait SExp[A]
+case class Atom[A](value: A) extends SExp[A]
+case class List[A](elements: List[SExp[A]]) extends SExp[A]
+
 class SimpleParseError
 
 case class SimpleParser[+A](run: String => Either[SimpleParseError, A]) {
