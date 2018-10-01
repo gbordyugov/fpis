778a33141ac4d0406148008f3001c1e6a9f1d396
Grigory Bordyugov <gregbg@gmail.com>
2018-10-01 09:34:16 +0200

    Chapter 9 don't import too much in tests

--- src/test/scala/fpis/chapter09/TestSExpParser.scala
+++ src/test/scala/fpis/chapter09/TestSExpParser.scala
@@ -1,8 +1,5 @@
 package fpis.chapter09.SExpParser
 
-import scala.language.{higherKinds, implicitConversions}
-
-import fpis.chapter09.{Parsers, ParseError, Location}
 import fpis.chapter09.SimpleParser.SimpleParsers
 
 object SExpParserTest {
