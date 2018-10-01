6af767a10ee84e03e3fc2975e1eb90a405017ec3
Grigory Bordyugov <gregbg@gmail.com>
2018-10-01 11:08:33 +0200

    Chapter 9 re-arranged code for tests

--- src/test/scala/fpis/chapter09/TestSimpleParsers.scala
+++ src/test/scala/fpis/chapter09/TestSimpleParsers.scala
@@ -2,33 +2,29 @@ package fpis.chapter09.SimpleParsers
 
 import org.scalatest._
 
-object TestMySimpleParsers {
-  import SimpleParsers._
-  import SimpleParser.Parser
-
-  def abra: Parser[String] = "abra"
-
-  val succ = run(abra)("abra")
-  val fail = run(abra)("abr")
-
-  def re: Parser[String] = regex("a+".r)
-
-  val succ_re = run(re)("abra")
-  val fail_re = run(re)("nopes")
-
-  def number: Parser[String] = regex("[0-9]+".r)
-  def a: Parser[String] = "a"
-  def nAs: Parser[Int] = number.map(_.toInt)
-
-  val flatMapSuccTest = run(number.map(_.toInt).flatMap(listOfN(_, a)))("4aaaa")
-  val flatMapFailTest = run(number.map(_.toInt).flatMap(listOfN(_, a)))("4aaa")
-
-  val parseManyA  = run("a".many )("aaaaa")
-  val parseMany1A = run("a".many1)("aaaaa")
-}
-
-
 class SimpleParsersTest extends FlatSpec {
   "bla" should "blo" in {
+    import SimpleParsers.{run => parserRun, _}
+    import SimpleParser.Parser
+
+    def abra: Parser[String] = "abra"
+
+    val succ = parserRun(abra)("abra")
+    val fail = parserRun(abra)("abr")
+
+    def re: Parser[String] = regex("a+".r)
+
+    val succ_re = parserRun(re)("abra")
+    val fail_re = parserRun(re)("nopes")
+
+    def number: Parser[String] = regex("[0-9]+".r)
+    def a: Parser[String] = "a"
+    def nAs: Parser[Int] = number.map(_.toInt)
+
+    val flatMapSuccTest = parserRun(number.map(_.toInt).flatMap(listOfN(_, a)))("4aaaa")
+    val flatMapFailTest = parserRun(number.map(_.toInt).flatMap(listOfN(_, a)))("4aaa")
+
+    val parseManyA  = parserRun("a".many )("aaaaa")
+    // val parseMany1A = parserRun("a".many1)("aaaaa")
   }
 }
