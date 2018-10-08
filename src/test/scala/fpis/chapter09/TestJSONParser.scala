3dc4c9cbbeecfa4273d6173a7cdb1e203ee3b14d
Grisha Bordyugov <gregbg@gmail.com>
2018-10-08 23:10:37 +0200

    Exercise 9.9 done

--- src/test/scala/fpis/chapter09/TestJSONParser.scala
+++ src/test/scala/fpis/chapter09/TestJSONParser.scala
@@ -8,7 +8,9 @@ import fpis.chapter09.JSONParser.JSONParser._
 import fpis.chapter09.JSONParser.JSON._
 
 
-
+/*
+ * This too belongs to Exercise 9.9
+ */
 class JSONParserTest extends FlatSpec {
   import SimpleParsers.{run => runParser, succeed => parseSucceed, _}
 
@@ -54,5 +56,71 @@ class JSONParserTest extends FlatSpec {
   it should "be able to parse an empty object" in {
     val result = runParser(parser)("{}")
     assert(result === Right(JObject(Map(): Map[String,JSON])))
+  }
+
+  it should "be able to parse an object with one field" in {
+    val result = runParser(parser)("{ \"bla\": 5}")
+    assert(result === Right(JObject(Map(("bla", JNumber(5))))))
+  }
+
+  it should "be able to parse an object with two fields" in {
+    val result = runParser(parser)("{ \"bla\": 5, \"bli\": \"blo\"}")
+    assert(result ===
+      Right(JObject(Map(("bla", JNumber(5)), ("bli", JString("blo"))))))
+  }
+
+  it should "be able to parse a slightly more complex object" in {
+    val string = "{ \"bla\": 5, \"bli\": \"blo\", \"blu\": [1, 2, {}], \"blk\": {\"a\": 1}}"
+    val result = runParser(parser)(string)
+    result match {
+      case Right(_) => assert(true)
+      case Left(_) => assert(false)
+    }
+  }
+
+  it should "be able to parse JSON schema" in {
+    val schema = """{
+  "$schema": "http://json-schema.org/schema#",
+  "title": "Product",
+  "type": "object",
+  "required": ["id", "name", "price"],
+  "properties": {
+    "id": {
+      "type": "number",
+      "description": "Product identifier"
+    },
+    "name": {
+      "type": "string",
+      "description": "Name of the product"
+    },
+    "price": {
+      "type": "number",
+      "minimum": 0
+    },
+    "tags": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    },
+    "stock": {
+      "type": "object",
+      "properties": {
+        "warehouse": {
+          "type": "number"
+        },
+        "retail": {
+          "type": "number"
+        }
+      }
+    }
+  }
+}
+"""
+    val result = runParser(parser)(schema)
+    result match {
+      case Right(_) => assert(true)
+      case Left(_) => assert(false)
+    }
   }
 }
