25b00658b4874b090fc358b04c2d4f8b11ecd5ca
Grigory Bordyugov <gregbg@gmail.com>
2018-10-08 09:25:58 +0200

    Chapter 9 checking in broken code of unit tests

--- src/test/scala/fpis/chapter09/TestSExpParser.scala
+++ src/test/scala/fpis/chapter09/TestSExpParser.scala
@@ -19,6 +19,12 @@ class SExpParserTest extends FlatSpec {
   val intAtom    = AtomInt(2345)
   val emptyList  = SExpList(List(): List[SExpression])
 
+  val one = AtomInt(1)
+  val two = AtomInt(2)
+  val tre = AtomInt(3)
+
+  val quoteSymbol = AtomSymbol("quote")
+
   "sExpParser" should "be able to parse simple symbols" in {
     val result = runParser(parser)("abcd")
     assert(result === Right(SExpAtom(abcdSymbol)))
@@ -41,11 +47,21 @@ class SExpParserTest extends FlatSpec {
 
   it should "be able to parse simple lists" in {
     val result = runParser(parser)("(1 2 3)")
-    println(result)
+    assert(result === Right(SExpList(List(SExpAtom(one), SExpAtom(two),
+      SExpAtom(tre)))))
   }
 
   it should "be able to parse nested lists" in {
     val result = runParser(parser)("(1 (quote 2 (3)))")
-    println(result)
+    val uno = SExpList(List(SExpAtom(AtomInt(3))))
+    val due = SExpList(List(SExpAtom(quoteSymbol), SExpAtom(two)))
+
+    /*n
+
+    assert(result === Right(SExpList(List(SExpAtom(AtomInt(1)),
+                                         SExpList(List(SExpAtom(AtomSymbol("quote")),
+                                                       SExpAtom(AtomInt(2)),
+                                                       SExpList(List(SExpAtom(AtomInt(3))))))))))
+     */
   }
 }
