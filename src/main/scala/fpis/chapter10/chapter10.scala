6f47351859649f825398c62e12d6f2b343d003ef
Grigory Bordyugov <gregbg@gmail.com>
2018-10-22 08:53:26 +0200

    Chapter 10 restructuring

--- src/main/scala/fpis/chapter10/chapter10.scala
+++ src/main/scala/fpis/chapter10/chapter10.scala
@@ -1,9 +1,11 @@
-object Chapter10 {
-  trait Monoid[A] {
-    def op(x: A, y: A): A
-    def zero: A
-  }
+package fpis.chapter10
 
+trait Monoid[A] {
+  def op(x: A, y: A): A
+  def zero: A
+}
+
+object Chapter10 {
   val stringMonoid = new Monoid[String] {
     def op(x: String, y: String) = x + y
     def zero = ""
