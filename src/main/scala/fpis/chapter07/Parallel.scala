110cac7cfd0ca5c1ccf13b4197d5c22a0c76e875
Grigory Bordyugov <gregbg@gmail.com>
2018-10-22 09:12:55 +0200

    Chapter 7: put tests in a separate file

--- src/main/scala/fpis/chapter07/Parallel.scala
+++ src/main/scala/fpis/chapter07/Parallel.scala
@@ -266,35 +266,3 @@ object Par {
     (f: (A, B) => C): Par[C] =
     flatMap(pa){a => flatMap(pb){b => unit(f(a, b))}}
 }
-
-
-
-object Examples {
-  val es = Executors.newFixedThreadPool(2)
-  def run[A](p: Par.Par[A]) = Par.run(es)(p).get
-
-  val p = Par.lazyUnit(1)
-
-  val things = (1 to 500).map(Par.lazyUnit(_)).toList
-  val thing = Par.sequence(things)
-
-  val anotherThing = Par.map2(Par.lazyUnit(1), Par.lazyUnit(1))(_ + _)
-
-  val p1 = Par.unit(1)
-  val p2 = Par.fork(p1)
-  val p3 = Par.fork(p2)
-  val p4 = Par.fork(p3)
-
-  val lst = Par.unit(List(3, 4, 1, 2))
-  val sortedLst = Par.map(lst)(_.sorted)
-
-  def sumSeq(s: Seq[Int]): Par.Par[Int] = {
-    val length = s.length
-    if (length <= 100)
-      Par.lazyUnit(s.foldRight(0)(_ + _))
-    else {
-      val (l, r) = s.splitAt(length/2)
-      Par.map2(sumSeq(l), sumSeq(r))(_ + _)
-    }
-  }
-}
---
+++ src/test/scala/fpis/chapter07/TestParallel.scala
@@ -0,0 +1,36 @@
+package fpis.chapter07
+
+import java.util.concurrent.{ExecutorService, Future, TimeUnit,
+  Callable, Executors}
+
+import org.scalatest._
+
+class ParallelTest extends FlatSpec {
+  val es = Executors.newFixedThreadPool(2)
+  def run[A](p: Par.Par[A]) = Par.run(es)(p).get
+
+  val p = Par.lazyUnit(1)
+
+  val things = (1 to 500).map(Par.lazyUnit(_)).toList
+  val thing = Par.sequence(things)
+
+  val anotherThing = Par.map2(Par.lazyUnit(1), Par.lazyUnit(1))(_ + _)
+
+  val p1 = Par.unit(1)
+  val p2 = Par.fork(p1)
+  val p3 = Par.fork(p2)
+  val p4 = Par.fork(p3)
+
+  val lst = Par.unit(List(3, 4, 1, 2))
+  val sortedLst = Par.map(lst)(_.sorted)
+
+  def sumSeq(s: Seq[Int]): Par.Par[Int] = {
+    val length = s.length
+    if (length <= 100)
+      Par.lazyUnit(s.foldRight(0)(_ + _))
+    else {
+      val (l, r) = s.splitAt(length/2)
+      Par.map2(sumSeq(l), sumSeq(r))(_ + _)
+    }
+  }
+}
