6d19b822c040fa4baac438697739ad84dcaaf797
Grisha Bordyugov <gregbg@gmail.com>
2018-10-31 13:57:43 +0100

    Chapter 11 renaming

--- src/main/scala/fpis/chapter11/Monads.scala
+++ src/main/scala/fpis/chapter11/Monads.scala
@@ -15,13 +15,12 @@ trait Functor[F[_]] {
     }
 }
 
-trait Mon[F[_]] {
+trait Monad[F[_]] extends Functor[F] {
   def unit[A](a: A): F[A]
+  def flatMap[A,B](fa: F[A])(f: A=>F[B]): F[B]
 
   def map[A,B](fa: F[A])(f: A=>B): F[B] =
     flatMap(fa) { a => unit(f(a)) }
-
-  def flatMap[A,B](fa: F[A])(f: A=>F[B]): F[B]
 
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B)=>C): F[C] =
     flatMap(fa) { a => map(fb) { b => f(a, b) } }
