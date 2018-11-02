cf72f1e9cb9fabea203f6b650e60cf4dcb96eaf5
Grigory Bordyugov <gregbg@gmail.com>
2018-11-02 14:31:21 +0100

    Chapter 11 two versions of genOrder()

--- src/main/scala/fpis/chapter11/Monad.scala
+++ src/main/scala/fpis/chapter11/Monad.scala
@@ -152,9 +152,19 @@ object Misc {
   case class Order(item: Item, quantity: Int)
   case class Item(name: String, price: Double)
 
-  val genorder: Gen[Order] = for{
+  val genOrder: Gen[Order] = for {
     name <- Gen.stringN(3)
     price <- Gen.uniform.map(_ * 10)
     quantity <- Gen.choose(1, 100)
   } yield Order(Item(name, price), quantity)
+
+  val genItem: Gen[Item] = for {
+    name <- Gen.stringN(3)
+    price <- Gen.uniform.map(_ * 10)
+  } yield Item(name, price)
+
+  val genOrderOther: Gen[Order] = for {
+    item <- genItem
+    quantity <- Gen.choose(1, 100)
+  } yield Order(item, quantity)
 }
