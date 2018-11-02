5c16c43d1079052947ebe871896c89b69e1837a6
Grigory Bordyugov <gregbg@gmail.com>
2018-11-02 15:44:12 +0100

    Exercise 11.8 done

--- src/main/scala/fpis/chapter11/Monad.scala
+++ src/main/scala/fpis/chapter11/Monad.scala
@@ -95,6 +95,14 @@ trait Monad[F[_]] extends Functor[F] {
    */
   def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] =
     a => flatMap(f(a))(g)
+
+  /*
+   * Exercise 11.8
+   */
+  def flatMapViaCompose[A,B](a: F[A])(f: A => F[B]): F[B] = {
+    val g: Unit => F[A] = _ => a
+    compose[Unit, A, B](g, f)(())
+  }
 }
 
 object Monad {
