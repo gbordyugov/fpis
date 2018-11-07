604e15f7abde110760c5bca9a75a92325d2148e3
Grigory Bordyugov <gregbg@gmail.com>
2018-11-07 10:16:36 +0100

    Chapter 12 Applicative.traverse()

--- src/main/scala/fpis/chapter12/Applicative.scala
+++ src/main/scala/fpis/chapter12/Applicative.scala
@@ -11,4 +11,7 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def map[A,B](fa: F[A])(f: A => B): F[B] =
     map2(fa, unit(()))((a, _) => f(a))
+
+  def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
 }
