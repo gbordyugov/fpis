5e2f776f96c664dee5db34ea3bfc233bad36492d
Grisha Bordyugov <gregbg@gmail.com>
2018-11-08 06:34:56 +0100

    chapter 12 Validation trait

--- src/main/scala/fpis/chapter12/Applicative.scala
+++ src/main/scala/fpis/chapter12/Applicative.scala
@@ -93,3 +93,10 @@ object Exercise125 {
       }
   }
 }
+
+sealed trait Validation[+E,+A]
+
+case class Failure[E](head: E,
+  tail: Vector[E] = Vector()) extends Validation[E, Nothing]
+
+case class Success[A](a: A) extends Validation[Nothing, A]
