587a3b2ab09db40d727fcdc08b8d8077cb638572
Grigory Bordyugov <gregbg@gmail.com>
2018-11-12 09:52:13 +0100

    Chapter 12 Traversable

--- src/main/scala/fpis/chapter12/Applicative.scala
+++ src/main/scala/fpis/chapter12/Applicative.scala
@@ -183,3 +183,12 @@ object ValidationApplicative {
 /*
  * Exercise 12.7 skipped
  */
+
+
+trait Traversable[F[_]] extends Functor[F] {
+  def traverse[G[_]: Applicative, A, B](fa: F[A])
+    (f: A => G[B]): G[F[B]] = sequence(map(fa)(f))
+
+  def sequence[G[_]:Applicative, A](fga: F[G[A]]): G[F[A]] =
+    traverse(fga)(ga => ga)
+}
