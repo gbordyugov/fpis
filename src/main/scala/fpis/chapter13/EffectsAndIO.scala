6465b779a30c8ccff11112dc042339d9912b94d8
Grigory Bordyugov <gregbg@gmail.com>
2018-11-20 08:51:33 +0100

    beginning of Chapter 13

--- src/main/scala/fpis/chapter13/EffectsAndIO.scala
+++ src/main/scala/fpis/chapter13/EffectsAndIO.scala
@@ -1,1 +1,29 @@
 package fpis.chapter13
+
+trait IO {
+  def run: Unit
+}
+
+object SimpleGame {
+  case class Player(name: String, score: Int)
+
+  def winner(p1: Player, p2: Player): Option[Player] =
+    if (p1.score > p2.score)
+      Some(p1)
+    else if (p2.score > p1.score)
+      Some(p2)
+    else
+      None
+
+  def winnerMsg(p: Option[Player]): String = p map {
+    case Player(name, _) => s"$name is the winner!"
+  } getOrElse "It's a draw."
+
+  def PrintLine(msg: String): IO =
+    new IO {
+      def run = println(msg)
+    }
+
+  def contest(p1: Player, p2: Player): IO =
+    PrintLine(winnerMsg(winner(p1, p2)))
+}
