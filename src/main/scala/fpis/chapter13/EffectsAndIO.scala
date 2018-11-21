ce37c81c6f94c31d1a04ceb81570d6f289bed14e
Grigory Bordyugov <gregbg@gmail.com>
2018-11-21 09:44:45 +0100

    Chapter 13 running our new IO monad

--- src/main/scala/fpis/chapter13/EffectsAndIO.scala
+++ src/main/scala/fpis/chapter13/EffectsAndIO.scala
@@ -84,4 +84,15 @@ object IO1 {
   case class Return[A](a: A) extends IO[A]
   case class Suspend[A](resume: () => A) extends IO[A]
   case class FlatMap[A,B](sub: IO[A], k: A => IO[B]) extends IO[B]
+
+  @annotation.tailrec
+  def run[A](io: IO[A]): A = io match {
+    case Return(a) => a
+    case Suspend(r) => r()
+    case FlatMap(x, f) => x match {
+      case Return(a) => run(f(a))
+      case Suspend(r) => run(f(r()))
+      case FlatMap(y, g) => run(y flatMap (a => g(a).flatMap(f)))
+    }
+  }
 }
