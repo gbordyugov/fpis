3ef877d9bd37e1d416bf074f5090af664149fae8
Grigory Bordyugov <gregbg@gmail.com>
2018-11-26 09:31:23 +0100

    Infrastructure to Async in Chapter 13

--- src/main/scala/fpis/chapter13/EffectsAndIO.scala
+++ src/main/scala/fpis/chapter13/EffectsAndIO.scala
@@ -1,5 +1,6 @@
 package fpis.chapter13
 
+import fpis.chapter07.Par
 import fpis.chapter07.Par.Par
 
 
@@ -147,4 +148,19 @@ object Async {
   case class FlatMap[A,B](sub: Async[A],
     k: A => Async[B]) extends Async[B]
 
+  @annotation.tailrec
+  def step[A](async: Async[A]): Async[A] = async match {
+    case FlatMap(FlatMap(x, f), g) => step(x.flatMap(a => f(a).flatMap(g)))
+    case FlatMap(Return(x), f)     => step(f(x))
+    case _                         => async
+  }
+
+  def run[A](async: Async[A]): Par[A] = step(async) match {
+    case Return(a) => Par.unit(a)
+    case Suspend(r) => r
+    case FlatMap(x, f) => x match {
+      case Suspend(r) => Par.flatMap(r)(a => run(f(a)))
+      case _          => sys.error("Impossible; `step` elimintates these cases")
+    }
+  }
 }
