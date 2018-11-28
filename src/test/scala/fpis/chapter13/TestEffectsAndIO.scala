a45f6c649d5d3abc701d61202dfd1c999a03ef2b
Grigory Bordyugov <gregbg@gmail.com>
2018-11-28 10:04:53 +0100

    Chapter 13 trying to understand trampolining

--- src/test/scala/fpis/chapter13/TestEffectsAndIO.scala
+++ src/test/scala/fpis/chapter13/TestEffectsAndIO.scala
@@ -5,10 +5,13 @@ import org.scalatest._
 class TestIO1 extends FlatSpec with Matchers {
   import IO1.{run => _, _}
   val x: TailRec[Int] = Return(1)
+
+  /*
   val func: TailRec[Int] =
     List.fill(100000)(1).foldLeft(x) { (x, _) =>
       x.flatMap(Return(_))
     }
+   */
 
   /*
    * this causes stack overflow error
@@ -18,5 +21,25 @@ class TestIO1 extends FlatSpec with Matchers {
   /*
    * this doesn't cause stack overflow error
    */
-  assert(1 === goodRun(func))
+  // assert(1 === goodRun(func))
+
+  def trivialize[A](a: TailRec[A]): TailRec[A] =
+    a.flatMap(Return[A](_))
+
+  @annotation.tailrec
+  final def myGoodRun[A](t: TailRec[A]): A = {
+    println(s"entering myGoodRun with t=$t")
+    t match {
+      case Return(x)  => x
+      case Suspend(f) => f()
+      case FlatMap(x, f) => x match {
+        case Return(y) => myGoodRun(f(y))
+        case Suspend(s) => myGoodRun(f(s()))
+        case FlatMap(y, g) => myGoodRun(y.flatMap(a => g(a).flatMap(f)))
+      }
+    }
+  }
+
+  val y = trivialize(trivialize(x))
+  assert(1 === myGoodRun(y))
 }
