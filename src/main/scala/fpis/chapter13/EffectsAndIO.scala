fac5bbab86ff89bf405f5877e944240e830e597e
Grigory Bordyugov <gregbg@gmail.com>
2018-12-03 10:03:33 +0100

    Chapter 13 baaad typo

--- src/main/scala/fpis/chapter13/EffectsAndIO.scala
+++ src/main/scala/fpis/chapter13/EffectsAndIO.scala
@@ -218,7 +218,7 @@ object Free {
   }
 
   case class Return[F[_],A](a: A) extends Free[F,A]
-  case class Susped[F[_],A](s: F[A]) extends Free[F,A]
+  case class Suspend[F[_],A](s: F[A]) extends Free[F,A]
   case class FlatMap[F[_],A,B](s: Free[F,A],
     f: A => Free[F,B]) extends Free[F,B]
 
