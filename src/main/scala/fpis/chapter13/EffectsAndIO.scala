831e252ab979ffb76b0fc9cc0d9aab0905939619
Grigory Bordyugov <gregbg@gmail.com>
2018-12-04 09:10:40 +0100

    Chapter 13 Console[A]

--- src/main/scala/fpis/chapter13/EffectsAndIO.scala
+++ src/main/scala/fpis/chapter13/EffectsAndIO.scala
@@ -266,4 +266,23 @@ object Free {
         case _ => ???
       }
     }
+
+  sealed trait Console[A] {
+    def toPar: Par[A]
+    def toThunk: () => A
+  }
+
+  case object ReadLine extends Console[Option[String]] {
+    def toPar = Par.lazyUnit(run)
+    def toThunk = () => run
+
+    def run: Option[String] =
+      try Some(readLine())
+      catch { case e: Exception => None }
+  }
+
+  case class PrintLine(line: String) extends Console[Unit] {
+    def toPar = Par.lazyUnit(println(line))
+    def toThunk = () => println(line)
+  }
 }
