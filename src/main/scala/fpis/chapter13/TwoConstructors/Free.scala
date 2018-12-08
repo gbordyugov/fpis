15d83e6c7aecb1d5ed8217874de6192042a307fa
Grisha Bordyugov <gregbg@gmail.com>
2018-12-08 21:38:58 +0100

    Chapter 13, Two ctor Free: fleshing out map and flatMap

--- src/main/scala/fpis/chapter13/TwoConstructors/Free.scala
+++ src/main/scala/fpis/chapter13/TwoConstructors/Free.scala
@@ -4,6 +4,18 @@ import scala.language.higherKinds
 
 import fpis.chapter13.Monad
 
-sealed trait Free[F[_],A]
+sealed trait Free[F[_],A] {
+  def flatMap[B](f: A => Free[F,B]): Free[F,B] = this match {
+    case Return(a)  => f(a)
+    case Suspend(f) => ???
+  }
+
+  def map[B](f: A => B): Free[F,B] =
+    flatMap(x => Return(f(x)))
+}
+
 case class Return[F[_],A](a: A) extends Free[F,A]
 case class Suspend[F[_],A](f: F[Free[F,A]]) extends Free[F,A]
+
+object Free {
+}
