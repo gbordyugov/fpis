eab409da92e8d2dfcb4ba60a5543979144663cd6
Grisha Bordyugov <gregbg@gmail.com>
2018-12-08 19:54:15 +0100

    working on my own implementation of Free

--- src/main/scala/fpis/chapter13/MyImplementation/Free.scala
+++ src/main/scala/fpis/chapter13/MyImplementation/Free.scala
@@ -2,8 +2,13 @@ package fpis.chapter13.MyImplementation
 
 import scala.language.higherKinds
 
-sealed trait Free[F[_],A]
+sealed trait Free[F[_],A] {
+  def flatMap[B](f: A => Free[F,B]): Free[F,B] =
+    FlatMap(this, f)
+  def map[B](f: A => B): Free[F,B] =
+    flatMap(x => Return(f(x)))
+}
 case class Return[F[_],A](a: A) extends Free[F,A]
 case class Suspend[F[_],A](fa: F[A]) extends Free[F,A]
-case class FlatMap[F[_],A,B](fa: Free[F,A])(f: A => Free[F,B])
+case class FlatMap[F[_],A,B](fa: Free[F,A], f: A => Free[F,B])
     extends Free[F,B]
