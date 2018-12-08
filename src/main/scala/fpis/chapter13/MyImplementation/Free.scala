718031626358bdf64f5811a85ec4479e52e1ab4c
Grisha Bordyugov <gregbg@gmail.com>
2018-12-08 20:03:28 +0100

    fleshing out Free.run()

--- src/main/scala/fpis/chapter13/MyImplementation/Free.scala
+++ src/main/scala/fpis/chapter13/MyImplementation/Free.scala
@@ -1,4 +1,5 @@
 package fpis.chapter13.MyImplementation
+import fpis.chapter13.Monad
 
 import scala.language.higherKinds
 
@@ -12,3 +13,21 @@ case class Return[F[_],A](a: A) extends Free[F,A]
 case class Suspend[F[_],A](fa: F[A]) extends Free[F,A]
 case class FlatMap[F[_],A,B](fa: Free[F,A], f: A => Free[F,B])
     extends Free[F,B]
+
+trait Translate[F[_],G[_]] {
+  def apply[A](a: F[A]): G[A]
+}
+
+object Free {
+  type ~>[F[_],G[_]] = Translate[F,G]
+
+  def run[F[_],A](f: Free[F,A])(implicit M: Monad[F]): F[A] = f match {
+    case Return(a)   => M.unit(a)
+    case Suspend(fa) => fa
+    case FlatMap(x, f) => x match {
+      case Return(a)     => run(f(a))
+      case Suspend(fa)   => ???
+      case FlatMap(y, g) => ???
+    }
+  }
+}
