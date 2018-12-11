90444b0968f1b6eab49f4b62ca86d79faf200981
Grigory Bordyugov <gregbg@gmail.com>
2018-12-11 10:02:09 +0100

    Chapter 13 what does the type signature of async() mean?

--- src/main/scala/fpis/chapter13/EffectsAndIO.scala
+++ src/main/scala/fpis/chapter13/EffectsAndIO.scala
@@ -384,6 +384,10 @@ object Exercise1305 {
 
   type Par[+A] = ExecutorService => Future[A]
 
+  /*
+   * this one expects a map for a callback (A => Unit) to Unit
+   * why?
+   */
   def async[A](run: (A => Unit) => Unit): Par[A] = es => new Future[A] {
     def apply(k: A => Unit) = run(k)
   }
