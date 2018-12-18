d44970dde008ddfde214a61f63ac29141909ad17
Grigory Bordyugov <gregbg@gmail.com>
2018-12-18 10:05:01 +0100

    Chapter 14 working on the unit test

--- src/test/scala/fpis/chapter14/TestST.scala
+++ src/test/scala/fpis/chapter14/TestST.scala
@@ -4,9 +4,17 @@ import org.scalatest._
 
 class TestST extends FlatSpec with Matchers {
   "bla" should "blo" in {
+    /*
+     * now that we can construct ST[_,_] values, we don't really know
+     * how to run them, since ST.run() is protected
+     */
     val test: ST[Nothing,(Int, Int)] = for {
       r1 <- STRef[Nothing,Int](1)
       r2 <- STRef[Nothing,Int](1)
+      x  <- r1.read
+      y  <- r2.read
+      _  <- r1.write(y+1)
+      _  <- r2.write(x+1)
       a  <- r1.read
       b  <- r2.read
     } yield (a, b)
