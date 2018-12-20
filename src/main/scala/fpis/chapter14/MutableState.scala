0093cd721a48bf7af41684b5c0a83fbc1979c42d
Grigory Bordyugov <gregbg@gmail.com>
2018-12-20 10:43:35 +0100

    Chapter 14 STArray

--- src/main/scala/fpis/chapter14/MutableState.scala
+++ src/main/scala/fpis/chapter14/MutableState.scala
@@ -83,3 +83,31 @@ trait RunnableST[A] {
    */
   def apply[S]: ST[S,A]
 }
+
+
+/*
+ * Scala requires an implicit Manifest cor constructing arrays
+ */
+sealed abstract class STArray[S,A](implicit m: Manifest[A]) {
+  protected def value: Array[A]
+
+  def size: ST[S,Int] = ST(value.size)
+
+  def write(i: Int, a: A): ST[S,Unit] = new ST[S,Unit] {
+    def run(s:S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  def read(i: Int): ST[S,A] = ST(value(i))
+
+  def freeze: ST[S,List[A]] = ST(value.toList)
+}
+
+object STArra {
+  def apply[S,A:Manifest](sz: Int, v: A): ST[S,STArray[S,A]] =
+    ST(new STArray[S,A] {
+      lazy val value = Array.fill(sz)(v)
+    })
+}
