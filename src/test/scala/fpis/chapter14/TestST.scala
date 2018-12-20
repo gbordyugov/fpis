012f19c632db05498bcd8223ebcecc93a7946b65
Grigory Bordyugov <gregbg@gmail.com>
2018-12-20 10:25:46 +0100

    Chapter 14 unit tests for RunnableST

--- src/test/scala/fpis/chapter14/TestST.scala
+++ src/test/scala/fpis/chapter14/TestST.scala
@@ -3,7 +3,7 @@ package fpis.chapter14
 import org.scalatest._
 
 class TestST extends FlatSpec with Matchers {
-  "bla" should "blo" in {
+  "ST" should "should be a monad" in {
     /*
      * now that we can construct ST[_,_] values, we don't really know
      * how to run them, since ST.run() is protected
@@ -20,11 +20,11 @@ class TestST extends FlatSpec with Matchers {
     } yield (a, b)
   }
 
-  "bli" should "blu" in {
+  "RunnableST" should "be computed correctly" in {
     val p = new RunnableST[(Int, Int)] {
       def apply[S] = for {
         r1 <- STRef[S,Int](1)
-        r2 <- STRef[S,Int](1)
+        r2 <- STRef[S,Int](2)
         x  <- r1.read
         y  <- r2.read
         _  <- r1.write(y+1)
@@ -33,5 +33,6 @@ class TestST extends FlatSpec with Matchers {
         b  <- r2.read
       } yield (a, b)
     }
+    assert (ST.runST(p) == (3, 2))
   }
 }
