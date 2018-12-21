846a45c1744c3e3fddceb4fd110fa4907d27c81e
Grigory Bordyugov <gregbg@gmail.com>
2018-12-21 11:50:19 +0100

    Chapter 14 quicksort

--- src/main/scala/fpis/chapter14/MutableState.scala
+++ src/main/scala/fpis/chapter14/MutableState.scala
@@ -1,5 +1,40 @@
 package fpis.chapter14
 
+object QuickSort {
+  def quicksort(xs: List[Int]): List[Int] = if (xs.isEmpty) xs else {
+    val arr = xs.toArray
+
+    def swap(i: Int, j: Int): Unit = {
+      val tmp = arr(i)
+      arr(i) = arr(j)
+      arr(j) = tmp
+    }
+
+    def partition(n: Int, r: Int, pivot: Int) = {
+      val pivotVal = arr(pivot)
+      swap(pivot, r)
+      var j = n
+      for (i <- n until r)
+        if (arr(i) < pivotVal) {
+          swap(i, j)
+          j += 1
+        }
+      swap(j, r)
+      j
+    }
+
+    def qs(n: Int, r: Int): Unit =
+      if (n < r) {
+        val pi = partition(n, r, n + (n - r)/2)
+        qs(n, pi - 1)
+        qs(pi + 1, r)
+      }
+
+    qs(0, arr.length - 1)
+    arr.toList
+  }
+}
+
 sealed trait ST[S,A] { self =>
   /*
    * note that the run() method is protected, contrarily to the normal
