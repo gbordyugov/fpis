8f674342ca0d25f4a6a5583cde50c736caa1e475
Grigory Bordyugov <gregbg@gmail.com>
2019-01-07 08:41:57 +0100

    Chapter 15 comments

--- src/main/scala/fpis/chapter15/Streams.scala
+++ src/main/scala/fpis/chapter15/Streams.scala
@@ -2,13 +2,33 @@ package fpis.chapter15
 
 /*
  * Transforms a stream containing I values into a stream containing O values
+ *
+ * This is a state machine that tells a further component (the driver) what
+ * to do next.
+ *
+ * So the driver consumes an instance of Process[I,O] *and* the input stream
+ * of values I (and probably generates an output stream of O).
  */
 sealed trait Process[I,O]
 
+/*
+ * Emit(head, tail) indicates to the driver that the `head` value
+ * should be emitted to the output stream, and the machine should then
+ * be transitioned to the `tail` state.
+ */
 case class Emit[I,O](head: O, tail: Process[I,O] = Halt[I,O])
     extends Process[I,O]
 
+/*
+ * Await(recv) requests a value from the input stream. The driver
+ * should pass the next available value to the `recv` function, or
+ * None if the input has no more elements.
+ */
 case class Await[I,O](recv: Option[I] => Process[I,O])
     extends Process[I,O]
 
+/*
+ * Halt indicates to the driver that no more elements should be read
+ * from the input or emitted to the output.
+ */
 case class Halt[I,O]() extends Process[I,O]
