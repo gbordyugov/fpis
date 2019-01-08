c413a2312c4dae3c8bd66ea498c46f902ffca9b8
Grigory Bordyugov <gregbg@gmail.com>
2019-01-08 08:50:44 +0100

    Chapter 15 shifting code around

--- src/main/scala/fpis/chapter15/Streams.scala
+++ src/main/scala/fpis/chapter15/Streams.scala
@@ -38,6 +38,28 @@ sealed trait Process[I,O] {
   }
 }
 
+/*
+ * Emit(head, tail) indicates to the driver that the `head` value
+ * should be emitted to the output stream, and the machine should then
+ * be transitioned to the `tail` state.
+ */
+case class Emit[I,O](head: O, tail: Process[I,O] = Halt[I,O])
+    extends Process[I,O]
+
+/*
+ * Await(recv) requests a value from the input stream. The driver
+ * should pass the next available value to the `recv` function, or
+ * None if the input has no more elements.
+ */
+case class Await[I,O](recv: Option[I] => Process[I,O])
+    extends Process[I,O]
+
+/*
+ * Halt indicates to the driver that no more elements should be read
+ * from the input or emitted to the output.
+ */
+case class Halt[I,O]() extends Process[I,O]
+
 object Process {
   def liftOne[I,O](f: I => O): Process[I,O] =
     Await {
@@ -62,26 +84,3 @@ object Process {
     go(0.0)
   }
 }
-
-/*
- * Emit(head, tail) indicates to the driver that the `head` value
- * should be emitted to the output stream, and the machine should then
- * be transitioned to the `tail` state.
- */
-case class Emit[I,O](head: O, tail: Process[I,O] = Halt[I,O])
-    extends Process[I,O]
-
-/*
- * Await(recv) requests a value from the input stream. The driver
- * should pass the next available value to the `recv` function, or
- * None if the input has no more elements.
- */
-case class Await[I,O](recv: Option[I] => Process[I,O])
-    extends Process[I,O]
-
-/*
- * Halt indicates to the driver that no more elements should be read
- * from the input or emitted to the output.
- */
-case class Halt[I,O]() extends Process[I,O]
-
