3d1e87772087705c5e3a150a4b978c172ccb2219
Grigory Bordyugov <gregbg@gmail.com>
2019-01-17 09:35:00 +0100

    Exercise 15.5 one more little assert

--- src/test/scala/fpis/chapter15/TestStream.scala
+++ src/test/scala/fpis/chapter15/TestStream.scala
@@ -77,6 +77,8 @@ class StreamTest extends FlatSpec with Matchers {
     val p2: Process[Int,Int] = lift(_ + 1)
     val p: Process[Int,Int] = p1 |> p2
 
+    assert(p2(Stream(0, 1, 2, 3)) === Stream(1, 2, 3, 4))
+
     assert(p(Stream(0, 1, 2, 3, 4)) === Stream(1, 3, 5))
   }
 }
