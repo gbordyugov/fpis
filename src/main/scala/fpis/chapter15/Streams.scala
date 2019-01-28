6c11e49de56bbc6b59b74cc9cd08551e3c222bbe
Grigory Bordyugov <gregbg@gmail.com>
2019-01-28 08:49:57 +0100

    Chapter 15 processFile()

--- src/main/scala/fpis/chapter15/Streams.scala
+++ src/main/scala/fpis/chapter15/Streams.scala
@@ -308,6 +308,20 @@ object Process {
   def exists[I](f: I=>Boolean): Process[I,Boolean] =
     Process.echo map f
 
-  def processFile[A,B](f: java.io.File, p: Process[String,A],
-    z: B): IO[B] = ???
+  def processFile[A,B](f: java.io.File, p: Process[String,A], z: B)
+      (g: (B,A) => B): IO[B] = IO {
+
+    @annotation.tailrec
+    def go(ss: Iterator[String], cur: Process[String,A], acc: B): B =
+      cur match {
+        case Halt() => acc
+        case Await(recv) =>
+          val next = if (ss.hasNext) recv(Some(ss.next)) else recv(None)
+          go(ss, next, acc)
+        case Emit(h, t) => go(ss, t, g(acc, h))
+      }
+    val s = io.Source.fromFile(f)
+    try go(s.getLines, p, z)
+    finally s.close
+  }
 }
