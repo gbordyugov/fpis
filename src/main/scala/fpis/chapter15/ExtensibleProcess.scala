2d22253065ab4b66c6d454ccae225963c9d6ce29
Grigory Bordyugov <gregbg@gmail.com>
2019-02-04 09:35:22 +0100

    Chapter 15 Extensible Process util functions

--- src/main/scala/fpis/chapter15/ExtensibleProcess.scala
+++ src/main/scala/fpis/chapter15/ExtensibleProcess.scala
@@ -3,7 +3,19 @@ package fpis.chapter15.ExtensibleProcess
 import scala.language.higherKinds
 
 trait Process[F[_],O] {
-  def onHalt(f: Throwable => Process[F,O]): Process[F,O] = ???
+  import Process._
+
+  def onHalt(f: Throwable => Process[F,O]): Process[F,O] = this match {
+    case Halt(e) => Try(f(e))
+    case Emit(h, t) => Emit(h, t.onHalt(f))
+    case Await(req, recv) => Await(req, recv andThen (_.onHalt(f)))
+  }
+
+  def ++(p: => Process[F,O]): Process[F,O] =
+    this.onHalt{
+      case End => p
+      case err => Halt(err)
+    }
 }
 
 object Process {
@@ -23,4 +35,10 @@ object Process {
    * Foreceful termination
    */
   case object Kill extends Exception
+
+  def Try[F[_],O](p: Process[F,O]): Process[F,O] =
+    try p
+    catch {
+      case e: Throwable => Halt(e)
+    }
 }
